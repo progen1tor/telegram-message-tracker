379557a28f8db54806961103ee886458ed8d8e33
ywz knnd <akmn140@gmail.com>
2026-07-20 15:29:02 +0300

    Modify constants

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,5 @@
 CONFIG_PATH = 'tg-message-tracker/config/config.json'
+
+ALL_MSG_LOG_FILEPATH = 'tg-message-tracker/logs/messages.log'
+TGT_MSG_LOG_FILEPATH = 'tg-message-tracker/logs/found_targets.log'
+EXP_LOG_FILEPATH = 'tg-message-tracker/logs/tracker.log'
