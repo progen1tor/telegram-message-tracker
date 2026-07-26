7506931550550619e5399e6494464c5bc1f32f39
ywz knnd <akmn140@gmail.com>
2026-07-26 15:53:16 +0300

    Add formatting func

--- src/tracker.py
+++ src/tracker.py
@@ -2,34 +2,34 @@ from telethon import TelegramClient, events, types
 import config 
 import logging_config as lc 
 from client import client
-from utils import kword_finder
+from utils import kword_finder, target_message_formatter
 
-chats = []  # plug 
 
-@client.on(events.NewMessage(chats=chats))
-async def message_tracker(event: events.NewMessage.Event) -> None: 
-    msg = event.message 
-    if (text:= msg.message):  
-        source = msg.peer_id 
-        if isinstance(source, types.PeerUser):
-            source_user = await client.get_entity(source.user_id)
-        elif isinstance(source, types.PeerChannel):
-            source_user = await client.get_entity(source.channel_id)
-        source_username = source_user.username if source_user.username else 'Unknown sourse'
-        dt = msg.date.strftime('%d.%m.%Y %I:%M:%S %p')
-        
-        if (kwords:= kword_finder(config.KEYWORDS, text)): 
-            try: 
-                user_id = msg.from_id.user_id 
-                user = await client.get_entity(user_id)
-                username = user.username if user.username else 'Unknown'
-            except AttributeError:
-                username = 'Unknown'
-            data = f'=== NEW MESSAGE WITH KEYWORDS FROM @{source_username} ===\nDATE: {dt}\nusername: @{username}\ntext: {text}\nkeywords found: {', '.join(kwords)}\n'
-            lc.target_messages_logger.info(data)
+def handlers_register(chats: list[int]):
+    @client.on(events.NewMessage(chats=chats))
+    async def message_tracker(event: events.NewMessage.Event) -> None: 
+        msg = event.message 
+        if (text:= msg.message):  
+            source = msg.peer_id 
+            if isinstance(source, types.PeerUser):
+                source_user = await client.get_entity(source.user_id)
+            elif isinstance(source, types.PeerChannel):
+                source_user = await client.get_entity(source.channel_id)
+            source_username = source_user.username if source_user.username else 'Unknown sourse'
+            dt = msg.date.strftime('%d.%m.%Y %I:%M:%S %p')
             
-            await client.send_message(config.NOTIFICATION_CHAT, data)
-            
-        else:   
-            data = f'New message from @{source_username}: {text} ({dt})'
-            lc.all_messages_logger.info(data)
+            if (kwords:= kword_finder(config.KEYWORDS, text)): 
+                try: 
+                    user_id = msg.from_id.user_id 
+                    user = await client.get_entity(user_id)
+                    username = user.username if user.username else 'Unknown'
+                except AttributeError:
+                    username = 'Unknown'
+                data = target_message_formatter(source_username, dt, username, text, kwords)
+                lc.target_messages_logger.info(data)
+                
+                await client.send_message(config.NOTIFICATION_CHAT, data)
+                
+            else:   
+                data = f'New message from @{source_username}: {text} ({dt})'
+                lc.all_messages_logger.info(data)
