447250b38ddcef60f34c3446bbeedbbf1fc39bc3
ywz knnd <akmn140@gmail.com>
2026-07-26 16:22:39 +0300

    Minor fix

--- src/utils.py
+++ src/utils.py
@@ -4,10 +4,9 @@ from telethon import TelegramClient
 async def chat_id_getter(client: TelegramClient, chat_list: list[str]) -> list[int]:
     ids = []
     
-    async with client: 
-        for chat in chat_list:
-            chat_entity = await client.get_entity(chat)
-            ids.append(chat_entity.id)
+    for chat in chat_list:
+        chat_entity = await client.get_entity(chat)
+        ids.append(chat_entity.id)
     
     return ids 
 
