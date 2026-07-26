ca06c8091e8b92798ebc7e713fbd012d8b1dca4b
ywz knnd <akmn140@gmail.com>
2026-07-26 15:41:31 +0300

    Add formatting func

--- src/utils.py
+++ src/utils.py
@@ -14,3 +14,13 @@ async def chat_id_getter(client: TelegramClient, chat_list: list[str]) -> list[i
 
 def kword_finder(kwords: list[str], text: str) -> list[str]: 
     return [w for w in kwords if w in text.lower()]
+
+
+def target_message_formatter(
+    source: str, 
+    dt: str, 
+    username: str,
+    msg_text: str, 
+    kwords: list[str]
+    ) -> str: 
+    return f'=== NEW MESSAGE WITH KEYWORDS FROM @{source} ===\nDATE: {dt}\nusername: @{username}\ntext: {msg_text}\nkeywords found: {', '.join(kwords)}\n'
