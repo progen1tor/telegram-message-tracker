98dd9bfb335a881ae73674a1b53940b16eb1486d
ywz knnd <akmn140@gmail.com>
2026-07-27 15:44:07 +0300

    Update tracker & add exceptions logging

--- src/tracker.py
+++ src/tracker.py
@@ -1,41 +1,55 @@
-from telethon import TelegramClient, events, types 
+from telethon import events, types, errors
+import asyncio 
 from . import config 
 from . import logging_config as lc 
 from .client import client
 from .utils import kword_finder, target_message_formatter
+from .notifier import message_sender
 
 
 def handlers_register(chats: list[int]):
     @client.on(events.NewMessage(chats=chats))
     async def message_tracker(event: events.NewMessage.Event) -> None: 
-        msg = event.message 
-        if (text:= msg.message):  
-            source = msg.peer_id 
+        try: 
             
-            source_username = None 
-            
-            if isinstance(source, types.PeerUser):
-                source_user = await client.get_entity(source.user_id)
-            elif isinstance(source, types.PeerChannel):
-                source_user = await client.get_entity(source.channel_id)
-            
-            if source_user:
-                source_username = source_user.username 
+            msg = event.message 
+            if (text:= msg.message):  
+                source = msg.peer_id 
                 
-            dt = msg.date.strftime('%d.%m.%Y %I:%M:%S %p')
-            
-            if (kwords:= kword_finder(config.KEYWORDS, text)): 
+                source_user = None 
+                source_username = None 
+                
                 try: 
-                    user_id = msg.from_id.user_id 
-                    user = await client.get_entity(user_id)
-                    username = user.username 
-                except (AttributeError, ValueError):
-                    username = None
-                data = target_message_formatter(source_username, dt, username, text, kwords)
-                lc.target_messages_logger.info(data)
+                    if isinstance(source, types.PeerUser):
+                        source_user = await client.get_entity(source.user_id)
+                    elif isinstance(source, types.PeerChannel):
+                        source_user = await client.get_entity(source.channel_id)
+                    elif isinstance(source, types.PeerChat):
+                        source_user = await client.get_entity(source.chat_id)
+                except (TypeError, ValueError) as exp:
+                    lc.exp_logger.error(f'({type(exp).__name__}) {exp}.')
                 
-                await client.send_message(config.NOTIFICATION_CHAT, data)
+                if source_user:
+                    source_username = source_user.username 
+                    
+                dt = msg.date.strftime('%d.%m.%Y %I:%M:%S %p')
                 
-            else:   
-                data = f'New message from @{source_username}: {text} ({dt})'
-                lc.all_messages_logger.info(data)
+                if (kwords:= kword_finder(config.KEYWORDS, text)): 
+                    try: 
+                        user_id = msg.from_id.user_id 
+                        user = await client.get_entity(user_id)
+                        username = user.username 
+                    except (AttributeError, ValueError):
+                        username = None
+                    data = target_message_formatter(source_username, dt, username, text, kwords)
+                    lc.target_messages_logger.info(data)
+                    
+                    await message_sender(client, config.NOTIFICATION_CHAT, data)
+                                        
+                else:   
+                    source_username = f'@{source_user.username}' if source_user and source_user.username else 'Unknown'
+                    data = f'New message from {source_username}: {text} ({dt})'
+                    lc.all_messages_logger.info(data)
+        
+        except Exception as exp:
+            lc.exp_logger.error(f'unexpected error ({type(exp).__name__}: {exp})')
