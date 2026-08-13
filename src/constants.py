fbd94d89d3c534958d239f15cf0ec626c6d5685e
ywz knnd <akmn140@gmail.com>
2026-08-13 09:56:35 +0300

    Fix paths

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,5 @@
-CONFIG_PATH = 'tg-message-tracker/config/config.json'
+CONFIG_PATH = 'config/config.json'
 
-ALL_MSG_LOG_FILEPATH = 'tg-message-tracker/logs/messages.log'
-TGT_MSG_LOG_FILEPATH = 'tg-message-tracker/logs/found_targets.log'
-EXP_LOG_FILEPATH = 'tg-message-tracker/logs/tracker.log'
+ALL_MSG_LOG_FILEPATH = 'logs/messages.log'
+TGT_MSG_LOG_FILEPATH = 'logs/found_targets.log'
+EXP_LOG_FILEPATH = 'logs/tracker.log'
